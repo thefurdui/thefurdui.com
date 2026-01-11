e9d7ea69d1633d38fbc1c1acf50d8a0a8c1227c3
Andrei <thefurdui@gmail.com>
2026-01-12 03:47:55 +0500

    deploy script updated

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 case $1 in 
     "deploy")
-        scp -r index.html style.css blog assets favicon root@128.199.255.153:/var/www/thefurdui
+        scp -r index.html style.css blog assets favicon zeus:/var/www/thefurdui
         echo "Deployed"
         ;;
     *)
